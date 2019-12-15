0b5916152f8721b2fd5f37df9727f56889ae4a5d
Vladimir Dvorak <lada.dvorak7@gmail.com>
2019-12-15 19:39:38 +0100

    Markdown

--- setup.py
+++ setup.py
@@ -4,10 +4,6 @@ from setuptools import setup, find_packages
 from udger import __version__
 
 
-with open(join(dirname(__file__), 'README.rst')) as readme_file:
-    long_description = readme_file.read()
-
-
 setup(
     name='udger',
     version=__version__,
@@ -15,7 +11,7 @@ setup(
     author='The Udger Team',
     author_email='info@udger.com',
     description="Fast and reliable User Agent parser and IP classifier for Python",
-    long_description=long_description,
+    long_description_content_type='text/markdown',
     url='https://github.com/udger/udger-python',
     packages=find_packages(
         exclude=('udger.tests',),
